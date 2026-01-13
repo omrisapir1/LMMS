da4ed3443318e7f3a53885384e556cccb4da1255
Omri <omri.sapir@workday.com>
2026-01-13 18:53:06 +0200

    all

--- phase1/train.py
+++ phase1/train.py
@@ -326,7 +326,7 @@ def train_phase1(config: Phase1Config) -> None:
                 )
 
             # Total loss
-            loss = loss_ans + 4 * loss_perm
+            loss = loss_ans + 1 * loss_perm
 
             if config.logg_loss_interval_batches > 0 and global_step % config.logg_loss_interval_batches == 0:
                 loss_ans_to_print = cur_answer_loss / config.logg_loss_interval_batches
