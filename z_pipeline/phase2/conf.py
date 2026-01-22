e7c5d9ff6d65cadf35d065ddb6dcb609094b4c35
Omri <omri.sapir@workday.com>
2026-01-22 22:37:23 +0200

    all

--- z_pipeline/phase2/conf.py
+++ z_pipeline/phase2/conf.py
@@ -95,7 +95,7 @@ class Phase2DataConfig:
 @dataclass
 class Phase2PretrainConfig:
     enable: bool = True
-    steps: int = 5000
+    steps: int = 10000
     lr: float = 5e-2
     temperature: float = 1.5
 
