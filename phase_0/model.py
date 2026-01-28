39f11edb495a6bceb320b03eb844847882f3cee1
Omri <omri.sapir@workday.com>
2026-01-28 10:46:03 +0200

    all

--- phase_0/model.py
+++ phase_0/model.py
@@ -46,7 +46,7 @@ class Phase0Model(PreTrainedModel):
             dtype=torch.bfloat16,
             trust_remote_code=True,
             output_hidden_states=True,
-            device_map='cuda',
+            device_map=None,
             low_cpu_mem_usage=False,
         )
 
