fd1af0b9bf37fe1610427c5976150546cfad9213
Omri <omri.sapir@workday.com>
2026-02-03 11:33:44 +0200

    all

--- z_pipeline/phase3/conf.py
+++ z_pipeline/phase3/conf.py
@@ -104,8 +104,8 @@ class Phase3OptimConfig:
 class Phase3TrainConfig:
     num_epochs: int = 30
     eval_every_steps: int = 5000
-    batch_size: int = 12
-    loss_batch_size: int = 3
+    batch_size: int = 8
+    loss_batch_size: int = 2
     gradient_accumulation_steps: int = 1
 
 
