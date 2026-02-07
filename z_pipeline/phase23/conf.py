f4b01f8fa1837ae957af259dd364b465a725edc5
Omri <omri.sapir@workday.com>
2026-02-07 20:59:38 +0200

    all

--- z_pipeline/phase23/conf.py
+++ z_pipeline/phase23/conf.py
@@ -49,8 +49,8 @@ class DataConfig:
 
 @dataclass
 class LossConfig:
-    lambda_ans: float = 1.0
-    lambda_sft: float = 0.1
+    lambda_ans: float = 0.5
+    lambda_sft: float = 0.05
     lambda_cf: float = 1.0
     lambda_batch: float = 0.0
     lambda_consistency: float = 0.0
