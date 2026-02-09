0bc7eb99392bf79a246f2ad997bd640273db29e3
Omri <omri.sapir@workday.com>
2026-02-09 13:41:23 +0200

    all

--- z_pipeline/phase23/train.py
+++ z_pipeline/phase23/train.py
@@ -283,7 +283,7 @@ def train(cfg: Config) -> None:
             if device.type == "cuda"
             else nullcontext()
         )
-        print(f'Example {tokenizer.decode(input_ids[0], skip_special_tokens=True)} -> {digit_labels[0]}')
+        print(f'Example {tokenizer.decode(input_ids[0], skip_special_tokens=False)} -> {digit_labels[0]}')
         with amp_ctx:
             out = model(
                 input_ids=input_ids,
