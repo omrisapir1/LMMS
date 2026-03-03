4559984dd783b09986a1e6b75a550c9a30ef5fcc
Omri <omri.sapir@workday.com>
2026-03-03 20:59:21 +0200

    all

--- SFT/config.py
+++ SFT/config.py
@@ -29,7 +29,7 @@ class SFTConfig:
     # Objective weights
     z_label_smoothing: float = 0.00
     w_z: float = 0.1
-    w_answer: float = 0.01
+    w_answer: float = 0.001
     w_digits: float = 1.0
 
     # Counterfactual dependence regularizer
