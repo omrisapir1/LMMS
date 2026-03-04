6cb13406e5cb193e6c6ce4b3dd61f9efbac73b4c
Omri <omri.sapir@workday.com>
2026-03-04 15:27:47 +0200

    all

--- SFT/config.py
+++ SFT/config.py
@@ -23,7 +23,7 @@ class SFTConfig:
     weight_decay: float = 0.0
     gradient_accumulation_steps: int = 1
     max_steps: int = 60_000
-    warmup_steps: int = 125
+    warmup_steps: int = 0
     max_length: int = 2048
 
     # Objective weights
