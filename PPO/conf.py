3d9cb6a0e17439b5758915ec6075f63e9c1b5830
Omri <omri.sapir@workday.com>
2026-03-08 09:49:29 +0200

    all

--- PPO/conf.py
+++ PPO/conf.py
@@ -29,7 +29,7 @@ class RolloutConfig:
     max_new_tokens: int = MAX_TOKENS
     temperature: float = 1.0
     top_p: float = 0.95
-    digit_greedy: bool = True
+    digit_greedy: bool = False
     action_scope: str = "ppo_full"  # "ppo_only_z_tokens" | "ppo_full"
     vllm_enabled: bool = True
     vllm_sync_every: int = 2
