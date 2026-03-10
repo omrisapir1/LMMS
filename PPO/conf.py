b324a80a23cdd3c25f4b5626cd92abecb8cbf214
Omri <omri.sapir@workday.com>
2026-03-10 14:35:20 +0200

    all

--- PPO/conf.py
+++ PPO/conf.py
@@ -60,6 +60,8 @@ class PPOConfig:
     clip_range: float = 0.2
     c_v: float = 0.5
     c_ent: float = 0.001
+    kl_coef: float = 0.02
+    update_ref_model_each_steps: int = 100
     ppo_epochs: int = 2
     minibatch_size: int = 32
     max_grad_norm: float = 1.0
