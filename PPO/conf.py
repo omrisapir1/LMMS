acde43be79062d8d2f6648720b05919cf808ba80
Omri <omri.sapir@workday.com>
2026-03-11 11:06:31 +0200

    all

--- PPO/conf.py
+++ PPO/conf.py
@@ -50,7 +50,7 @@ class RewardConfig:
     partial_scale: float = 0.5
     keep_prob: Tuple[float, float, float, float, float] = (0.02, 0.05, 0.1, 0.5, 1.0)
     length_penalty: float = 0.001
-    reward_if_max_len: float = 0.0
+    reward_if_max_len: float = -1
 
 
 @dataclass
@@ -58,7 +58,7 @@ class PPOConfig:
     clip_range: float = 0.2
     c_v: float = 0.5
     c_ent: float = 0.000
-    kl_coef: float = 0.02
+    kl_coef: float = 0.00
     update_ref_model_each_steps: int = 500
     ppo_epochs: int = 2
     minibatch_size: int = 16
@@ -68,7 +68,7 @@ class PPOConfig:
 
 @dataclass
 class TrainConfig:
-    lr: float = 3e-5
+    lr: float = 2e-5
     weight_decay: float = 0.0
     betas: Tuple[float, float] = (0.9, 0.95)
     eps: float = 1e-8
