5eff176501382ffe56edfd805898f9e7cdce10b4
Omri <omri.sapir@workday.com>
2026-03-18 12:02:26 +0200

    all

--- PPO/conf.py
+++ PPO/conf.py
@@ -72,7 +72,7 @@ class PPOConfig:
 
 @dataclass
 class TrainConfig:
-    lr: float = 1e-5
+    lr: float = 3e-6
     weight_decay: float = 0.0
     betas: Tuple[float, float] = (0.9, 0.95)
     eps: float = 1e-8
