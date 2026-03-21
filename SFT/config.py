2dc352336f7740b60258f330b87aaae9042fe5a1
Omri <omri.sapir@workday.com>
2026-03-21 15:20:21 +0200

    all

--- SFT/config.py
+++ SFT/config.py
@@ -17,7 +17,7 @@ class SFTConfig:
 
     # Training
     seed: int = 42
-    batch_size: int = 4
+    batch_size: int = 8
     eval_batch_size: int = 1024
     learning_rate: float = 2e-5
     weight_decay: float = 0.0
