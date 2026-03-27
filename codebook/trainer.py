abd529cfe172750a97d35eeecb470e75c9853cee
Omri <omri.sapir@workday.com>
2026-03-27 12:49:03 +0300

    [build]

--- codebook/trainer.py
+++ codebook/trainer.py
@@ -38,7 +38,7 @@ class CodebookTrainer:
         self.config = config
         _set_seed(config.seed)
 
-        self.device = torch.device("cpu")#("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda")#("cuda" if torch.cuda.is_available() else "cpu")
         self.model = Codebook(
             dim=config.dim,
             vocab_size=config.vocab_size,
