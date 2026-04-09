410fb49b4e7a5a991441485c17fda9230ae2a3dc
Omri <omri.sapir@workday.com>
2026-04-09 14:14:14 +0300

    [build]

--- RSFT/config.py
+++ RSFT/config.py
@@ -94,7 +94,7 @@ class LossConfig:
 @dataclass
 class EvalConfig:
     eval_every_steps: int = 100000
-    eval_at_start: bool = True
+    eval_at_start: bool = False
     vllm_batch_size: int = 256
     pass_at_n: int = 16
     k_max: int = 2048
