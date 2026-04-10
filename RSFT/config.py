d8f2c846b4166e4ad2d76c814b12df95aa9656f4
Omri <omri.sapir@workday.com>
2026-04-10 10:25:06 +0300

    [build]

--- RSFT/config.py
+++ RSFT/config.py
@@ -6,7 +6,7 @@ from typing import Any, Dict, Optional, Sequence, Tuple
 
 @dataclass
 class ModelConfig:
-    init_ckpt: str = "omrisap/nemotron-7B-12K"
+    init_ckpt: str = "omrisap/RSFT_250_8"
     answer_token: str = "<ANSWER>"
     finalize_token: str = "<FINALIZE>"
     retry_token: str = "<RETRY>"
