c64b63ac6733cf4f11a3188b0421beab5d889117
Omri <omri.sapir@workday.com>
2026-04-19 11:05:20 +0300

    [build]

--- GRPO/conf.py
+++ GRPO/conf.py
@@ -66,7 +66,7 @@ class GRPOConfig:
     clip_range: float = 0.2
     c_ent: float = 0.001
     ppo_epochs: int = 1
-    minibatch_size: int = 16
+    minibatch_size: int = 8
     max_grad_norm: float = 1.0
 
 
