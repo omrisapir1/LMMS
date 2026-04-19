de38d07e6d7a34cd78b55c41e8e506939fb92b3f
Omri <omri.sapir@workday.com>
2026-04-19 18:11:46 +0300

    [build]

--- GRPO/conf.py
+++ GRPO/conf.py
@@ -74,7 +74,7 @@ class GRPOConfig:
 
 @dataclass
 class TrainConfig:
-    lr: float = 1e-4
+    lr: float = 5e-5
     weight_decay: float = 0.0
     betas: Tuple[float, float] = (0.9, 0.95)
     eps: float = 1e-8
