d101b86d03a216fcead50f40b33c0bbf272b0eb5
Omri <omri.sapir@workday.com>
2026-04-20 19:33:37 +0300

    [build]

--- TREE_GRPO/conf.py
+++ TREE_GRPO/conf.py
@@ -104,7 +104,7 @@ class PPOConfig:
 
 @dataclass
 class TrainConfig:
-    lr: float = 3e-5
+    lr: float = 2e-5
     weight_decay: float = 0.0
     betas: Tuple[float, float] = (0.9, 0.95)
     eps: float = 1e-8
